483a7625832d6d338056cd0c85bf5ed1f854279a
Robert Schwenn <devel@rstyx.de>
2014-06-22 15:29:25 +0200

    Added TopologyNode

--- Utilities/source/Domain/PositionAtTopology.vb
+++ Utilities/source/Domain/PositionAtTopology.vb
@@ -4,35 +4,17 @@ Namespace Domain
     ''' <summary> Position description related to a topology (node-edge model). </summary>
     Public Class PositionAtTopology
         
-        ''' <summary> Code or shortcut title of A-node. </summary>
-        Public Property ANodeCode           As String = String.Empty
+        ''' <summary> A-node. </summary>
+        Public Property NodeA               As TopologyNode = Nothing
         
-        ''' <summary> Title of A-node. </summary>
-        Public Property ANodeTitle          As String = String.Empty
+        ''' <summary> B-node. </summary>
+        Public Property NodeB               As TopologyNode = Nothing
         
-        ''' <summary> Description of A-node. </summary>
-        Public Property ANodeDescription    As String = String.Empty
+        ''' <summary> Distance of A-node to position. </summary>
+        Public Property DistanceToNodeA     As Double = Double.NaN
         
-        ''' <summary> Distance of A-node to bottleneck. </summary>
-        Public Property ANodeDistance       As Double = Double.NaN
-        
-        ''' <summary> Operation point of A-node. </summary>
-        Public Property ANodeOperationPoint As String = String.Empty
-        
-        ''' <summary> Code or shortcut title of B-Node. </summary>
-        Public Property BNodeCode           As String = String.Empty
-        
-        ''' <summary> Title of B-Node. </summary>
-        Public Property BNodeTitle          As String = String.Empty
-        
-        ''' <summary> Description of B-Node. </summary>
-        Public Property BNodeDescription    As String = String.Empty
-        
-        ''' <summary> Distance of B-Node to bottleneck. </summary>
-        Public Property BNodeDistance       As Double = Double.NaN
-        
-        ''' <summary> Operation point of B-Node. </summary>
-        Public Property BNodeOperationPoint As String = String.Empty
+        ''' <summary> Distance of B-Node to position. </summary>
+        Public Property DistanceToNodeB     As Double = Double.NaN
         
     End Class
     
---
+++ Utilities/source/Domain/TopologyNode.vb
@@ -0,0 +1,23 @@
+﻿
+Namespace Domain
+    
+    ''' <summary> A Node of a topology (node-edge model). </summary>
+    Public Class TopologyNode
+        
+        ''' <summary> Code (or shortcut title) of the node. </summary>
+        Public Property Code            As String = String.Empty
+        
+        ''' <summary> Title of the node. </summary>
+        Public Property Title           As String = String.Empty
+        
+        ''' <summary> Description of the node. </summary>
+        Public Property Description     As String = String.Empty
+        
+        ''' <summary> Operation point the node belogs to. </summary>
+        Public Property OperationPoint  As String = String.Empty
+        
+    End Class
+    
+End Namespace
+
+' for jEdit:  :collapseFolds=3::tabSize=4::tabSize=4:
