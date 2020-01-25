73e2413bd58268f932ae50c06a37b214813f557c
Robert Schwenn <devel@rstyx.de>
2020-01-25 23:43:12 +0100

    New extension Boolean.TryParse() accepts localized strings from resources.

--- Utilities/source/_global/Extensions.vb
+++ Utilities/source/_global/Extensions.vb
@@ -2,6 +2,7 @@
 Imports System
 Imports System.Linq
 Imports System.Collections.Generic
+Imports System.Runtime.InteropServices
 
 'Namespace Extensions
     
@@ -52,11 +53,51 @@ Imports System.Collections.Generic
         End Function
         
         ''' <summary> Converts this Boolean value to a string localized by resources. </summary>
+         ''' <returns> <see cref="Rstyx.Utilities.Resources.Messages.Global_Label_BooleanTrue"/> or <see cref="Rstyx.Utilities.Resources.Messages.Global_Label_BooleanFalse"/>. </returns>
          ''' <param name="Value"> The boolean value to convert. </param>
         <System.Runtime.CompilerServices.Extension()> 
         Public Function ToDisplayString(Value As Boolean) As String
             Return If(Value, Rstyx.Utilities.Resources.Messages.Global_Label_BooleanTrue, Rstyx.Utilities.Resources.Messages.Global_Label_BooleanFalse)
         End Function
+            
+            ''' <summary> Replacement for <c>Boolean.TryParse</c> which converts strings localized by resources. </summary>
+             ''' <param name="Result"> The parsing result. </param>
+             ''' <param name="Value">  String to parse. </param>
+             ''' <returns> <see langword="true"/> if <paramref name="Value"/> has been parsed successfull, otherwise <see langword="false"/>. </returns>
+             ''' <remarks>
+             ''' If <c>Boolean.TryParse</c> fails, then special parsing will be done for 
+             ''' <see cref="Rstyx.Utilities.Resources.Messages.Global_Label_BooleanTrue"/> and
+             ''' <see cref="Rstyx.Utilities.Resources.Messages.Global_Label_BooleanFalse"/> (not case sensitive).
+             ''' If <paramref name="Value"/> contains is only one character, it will be successfuly parsed 
+             ''' if it matches the first character of one of the resorce strings.
+             ''' </remarks>
+            <System.Runtime.CompilerServices.Extension()> 
+            Public Function TryParse(<out> ByRef Result As Boolean, Value As String) As Boolean
+                Dim success As Boolean = False
+                
+                If (Value.IsNotEmptyOrWhiteSpace()) Then
+                    
+                    success = Boolean.TryParse(Value, Result)
+                    
+                    If (Not success) Then
+                        Select Case Value.ToLowerInvariant()
+                            Case Rstyx.Utilities.Resources.Messages.Global_Label_BooleanTrue.ToLower()  :  Result = True  : success = True
+                            Case Rstyx.Utilities.Resources.Messages.Global_Label_BooleanFalse.ToLower() :  Result = False : success = True
+                        End Select
+                    End If
+                    
+                    If (Not success) Then
+                        If (Value.Length = 1) Then
+                            Select Case Value.Left(1).ToLowerInvariant()
+                                Case Rstyx.Utilities.Resources.Messages.Global_Label_BooleanTrue.Left(1).ToLower()  :  Result = True  : success = True
+                                Case Rstyx.Utilities.Resources.Messages.Global_Label_BooleanFalse.Left(1).ToLower() :  Result = False : success = True
+                            End Select
+                        End If
+                    End If
+                End If
+                
+                Return success
+            End Function
         
     End Module
     
