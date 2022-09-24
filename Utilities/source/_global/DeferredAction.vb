acc868876f620252b859a7d01e2cacc020abfe01
Robert Schwenn <devel@rstyx.de>
2022-09-24 21:28:28 +0200

    DeferredAction: Added ability to specify a maximum delay.

--- Utilities/source/_global/DeferredAction.vb
+++ Utilities/source/_global/DeferredAction.vb
@@ -1,10 +1,13 @@
 ﻿
 Imports System
+Imports System.Diagnostics
+Imports System.Threading
+Imports System.Windows.Threading
 
-''' <summary>  A timer that performs an action on a certain thread when time elapses. Rescheduling is supported. </summary>
+''' <summary>  A DeferTimer that performs an Action on a certain thread when time elapses. Rescheduling is supported. </summary>
  ''' <remarks> 
  ''' <para>
- ''' By default that works on the current thread. By specifying a dispatcher another thread can be determined.
+ ''' By default that works on the current thread. By specifying a Dispatcher another thread can be determined.
  ''' </para>
  ''' <para>
  ''' Origin: <a href="http://www.codeproject.com/KB/WPF/SnappyFiltering.aspx" target="_blank"> "Deferring ListCollectionView filter updates for a responsive UI" by Matt T Hayes </a> 
@@ -22,34 +25,75 @@ Imports System
  ''' Specified thread (WPF Application's UI tread in this case): <c>Dim DeferredScrollAction As DeferredAction = New DeferredAction(AddressOf scrollToEndOfLog, System.Windows.Application.Current.Dispatcher)</c>
  ''' </para>
  ''' <para>
- ''' Schedule the action: <c>DeferredScrollAction.Defer(TimeSpan.FromMilliseconds(100))</c>
+ ''' Schedule the Action: <c>DeferredScrollAction.Defer(TimeSpan.FromMilliseconds(100))</c>
  ''' </para>
  ''' </remarks>
 Public Class DeferredAction
     Implements IDisposable
     
-    Private timer As System.Threading.Timer
+    Protected DeferTimer As Timer
+
+    Protected ReadOnly IsStatusSupported As Boolean
+    Protected ReadOnly DelayCounter      As Stopwatch
+
+    #Region "Fields"
+        
+        ''' <summary> Gets or sets the status of this DeferredAction: <see langword="true"/>, if execution of an action is scheduled. </summary>
+         ''' <returns> Status of currently deferring an action. </returns>
+         ''' <remarks>
+         ''' This is set to <see langword="true"/>, when <see cref="Defer"/> is called. 
+         ''' This property is meaningfull only if the deferred action is of type <see cref="Action(Of DeferredAction)"/>,
+         ''' which gets this DeferredAction as argument and set this property to <see langword="false"/>, after finished. 
+         ''' Otherwise, a given maximum delay can't be respected.
+         ''' </remarks>
+        Public Property IsDeferring As Boolean = False
+        
+    #End Region
     
     #Region "Constuctor"
         
         ''' <summary> Creates a new DeferredAction running in current thread. </summary>
-         ''' <param name="action"> The action that is intended to be invoked deferred. </param>
-         ''' <exception cref="System.ArgumentNullException"> <paramref name="action"/> is <see langword="null"/>. </exception>
-        Public Sub New(action As Action)
-            Me.New(action, System.Windows.Threading.Dispatcher.CurrentDispatcher)
+         ''' <param name="Action"> The Action that is intended to be invoked deferred. </param>
+         ''' <exception cref="ArgumentNullException"> <paramref name="Action"/> is <see langword="null"/>. </exception>
+        Public Sub New(Action As Action)
+            Me.New(Action, Dispatcher.CurrentDispatcher)
         End Sub
         
         ''' <summary> Creates a new DeferredAction running in a given thread. </summary>
-         ''' <param name="action">     The action that is intended to be invoked deferred. </param>
-         ''' <param name="dispatcher"> The dispatcher that will invoke the action (when time has come). </param>
-         ''' <exception cref="System.ArgumentNullException"> <paramref name="action"/> is <see langword="null"/>. </exception>
-         ''' <exception cref="System.ArgumentNullException"> <paramref name="dispatcher"/> is <see langword="null"/>. </exception>
-        Public Sub New(action As Action, dispatcher As System.Windows.Threading.Dispatcher)
+         ''' <param name="Action">     The Action that is intended to be invoked deferred. </param>
+         ''' <param name="Dispatcher"> The Dispatcher that will invoke the Action (when time has come). </param>
+         ''' <exception cref="ArgumentNullException"> <paramref name="Action"/> is <see langword="null"/>. </exception>
+         ''' <exception cref="ArgumentNullException"> <paramref name="Dispatcher"/> is <see langword="null"/>. </exception>
+        Public Sub New(Action As Action, Dispatcher As Dispatcher)
             
-            If (action Is Nothing) Then Throw New ArgumentNullException("action")
-            If (dispatcher Is Nothing) Then Throw New ArgumentNullException("dispatcher")
+            If (Action Is Nothing) Then Throw New ArgumentNullException("action")
+            If (Dispatcher Is Nothing) Then Throw New ArgumentNullException("dispatcher")
             
-            Me.timer = New System.Threading.Timer(New System.Threading.TimerCallback(Sub() dispatcher.Invoke(action) ))
+            Me.DeferTimer     = New Timer(New TimerCallback(Sub() Dispatcher.Invoke(Action) ))
+            IsStatusSupported = False
+        End Sub
+        
+
+        ''' <summary> Creates a new DeferredAction running in current thread. </summary>
+         ''' <param name="Action"> The Action that is intended to be invoked deferred and to signal execution. </param>
+         ''' <exception cref="ArgumentNullException"> <paramref name="Action"/> is <see langword="null"/>. </exception>
+        Public Sub New(Action As Action(Of DeferredAction))
+            Me.New(Action, Dispatcher.CurrentDispatcher)
+        End Sub
+        
+        ''' <summary> Creates a new DeferredAction running in a given thread. </summary>
+         ''' <param name="Action">     The Action that is intended to be invoked deferred and to signal execution. </param>
+         ''' <param name="Dispatcher"> The Dispatcher that will invoke the Action (when time has come). </param>
+         ''' <exception cref="ArgumentNullException"> <paramref name="Action"/> is <see langword="null"/>. </exception>
+         ''' <exception cref="ArgumentNullException"> <paramref name="Dispatcher"/> is <see langword="null"/>. </exception>
+        Public Sub New(Action As Action(Of DeferredAction), Dispatcher As Dispatcher)
+            
+            If (Action Is Nothing) Then Throw New ArgumentNullException("Action")
+            If (Dispatcher Is Nothing) Then Throw New ArgumentNullException("Dispatcher")
+            
+            Me.DeferTimer     = New Timer(New TimerCallback(Sub() Dispatcher.Invoke(Action, Me) ))
+            IsStatusSupported = True
+            DelayCounter      = New Stopwatch()
         End Sub
         
     #End Region
@@ -57,33 +101,83 @@ Public Class DeferredAction
     #Region "Public Methods"
         
         ''' <summary>
-        ''' Schedules the action for performing once after the specified delay.
-        ''' Repeated calls will reschedule the action, if it has not already been performed.
+        ''' Schedules the Action for performing once after the specified Delay.
+        ''' Repeated calls will reschedule the Action, if it has not already been performed.
         ''' </summary>
-         ''' <param name="delay"> The amount of time to wait before performing the action. </param>
-         ''' <exception cref="System.ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's timer) has been already disposed of. </exception>
-         ''' <exception cref="System.ArgumentOutOfRangeException"> <paramref name="delay"/> is less than -1. </exception>
-         ''' <exception cref="System.NotSupportedException">       <paramref name="delay"/> is greater than 4294967294 milliseconds. </exception>
-        Public Sub Defer(delay As TimeSpan)
-            Me.timer.Change(delay, TimeSpan.FromMilliseconds(-1))
+         ''' <param name="Delay"> The amount of time (in milliseconds) to wait before performing the Action. </param>
+         ''' <exception cref="ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's DeferTimer) has been already disposed of. </exception>
+         ''' <exception cref="ArgumentOutOfRangeException"> <paramref name="Delay"/> is less than -1. </exception>
+         ''' <exception cref="NotSupportedException">       <paramref name="Delay"/> is greater than 4294967294 milliseconds. </exception>
+        Public Sub Defer(Delay As Double)
+            Me.Defer(TimeSpan.FromMilliseconds(Delay))
         End Sub
         
         ''' <summary>
-        ''' Schedules the action for performing once after the specified delay.
-        ''' Repeated calls will reschedule the action, if it has not already been performed.
+        ''' Schedules the Action for performing once after the specified Delay.
+        ''' Repeated calls will reschedule the Action, if it has not already been performed.
         ''' </summary>
-         ''' <param name="delay"> The amount of time (in milliseconds) to wait before performing the action. </param>
-         ''' <exception cref="System.ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's timer) has been already disposed of. </exception>
-         ''' <exception cref="System.ArgumentOutOfRangeException"> <paramref name="delay"/> is less than -1. </exception>
-         ''' <exception cref="System.NotSupportedException">       <paramref name="delay"/> is greater than 4294967294 milliseconds. </exception>
-        Public Sub Defer(delay As Double)
-            Me.timer.Change(TimeSpan.FromMilliseconds(delay), TimeSpan.FromMilliseconds(-1))
+         ''' <param name="Delay"> The amount of time to wait before performing the Action. </param>
+         ''' <exception cref="ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's DeferTimer) has been already disposed of. </exception>
+         ''' <exception cref="ArgumentOutOfRangeException"> <paramref name="Delay"/> is less than -1. </exception>
+         ''' <exception cref="NotSupportedException">       <paramref name="Delay"/> is greater than 4294967294 milliseconds. </exception>
+        Public Sub Defer(Delay As TimeSpan)
+            Me.DeferTimer.Change(Delay, TimeSpan.FromMilliseconds(-1))
         End Sub
         
+        ''' <summary>
+        ''' Schedules the Action for performing once after the specified Delay.
+        ''' Repeated calls will reschedule the Action, if it has not already been performed.
+        ''' If the action supports status monitoring, then it's invoked after the specified <paramref name="MaxDelay"/>, 
+        ''' even if it would have been rescheduled otherwise.
+        ''' </summary>
+         ''' <param name="Delay"> The amount of time (in milliseconds) to wait before performing the Action. </param>
+         ''' <param name="MaxDelay"> The maximum amount of time (in milliseconds) to wait before performing the Action. </param>
+         ''' <exception cref="ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's DeferTimer) has been already disposed of. </exception>
+         ''' <exception cref="ArgumentOutOfRangeException"> <paramref name="Delay"/> is less than -1. </exception>
+         ''' <exception cref="NotSupportedException">       <paramref name="Delay"/> is greater than 4294967294 milliseconds. </exception>
+         ''' <exception cref="ArgumentException">           <paramref name="MaxDelay"/> isn't greater than <paramref name="Delay"/>. </exception>
+        Public Sub Defer(Delay As Double, MaxDelay As Double)
+            Me.DeferTimer.Change(TimeSpan.FromMilliseconds(Delay), TimeSpan.FromMilliseconds(-1))
+        End Sub
+        
+        ''' <summary>
+        ''' Schedules the Action for performing once after the specified Delay.
+        ''' Repeated calls will reschedule the Action, if it has not already been performed.
+        ''' If the action supports status monitoring, then it's invoked after the specified <paramref name="MaxDelay"/>, 
+        ''' even if it would have been rescheduled otherwise.
+        ''' </summary>
+         ''' <param name="Delay"> The amount of time to wait before performing the Action. </param>
+         ''' <param name="MaxDelay"> The maximum amount of time to wait before performing the Action. </param>
+         ''' <exception cref="ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's DeferTimer) has been already disposed of. </exception>
+         ''' <exception cref="ArgumentOutOfRangeException"> <paramref name="Delay"/> is less than -1. </exception>
+         ''' <exception cref="NotSupportedException">       <paramref name="Delay"/> is greater than 4294967294 milliseconds. </exception>
+         ''' <exception cref="ArgumentException">           <paramref name="MaxDelay"/> isn't greater than <paramref name="Delay"/>. </exception>
+        Public Sub Defer(Delay As TimeSpan, MaxDelay As TimeSpan)
+
+            If (MaxDelay <= Delay) Then Throw New System.ArgumentException("MaxDelay")
+
+            If (IsStatusSupported) Then
+                If (Me.IsDeferring) Then
+                    If (DelayCounter.ElapsedMilliseconds > MaxDelay.Milliseconds) Then
+                        ' Execute now, hence schedule in 1 millisecond.
+                        Me.IsDeferring = False
+                        Delay = TimeSpan.FromMilliseconds(0)
+                    End If
+                Else
+                    Me.IsDeferring = True
+                    DelayCounter.Restart()
+                End If
+            End If
+
+            Me.DeferTimer.Change(Delay, TimeSpan.FromMilliseconds(-1))
+        End Sub
+        
+
         ''' <summary> Aborts an active schedule, if there is one. </summary>
-         ''' <exception cref="System.ObjectDisposedException"> This <see cref="DeferredAction"/> (resp. it's timer) has been already disposed of. </exception>
+         ''' <exception cref="ObjectDisposedException"> This <see cref="DeferredAction"/> (resp. it's DeferTimer) has been already disposed of. </exception>
         Public Sub Abort()
-            Me.timer.Change(TimeSpan.FromMilliseconds(-1), TimeSpan.FromMilliseconds(-1))
+            Me.DeferTimer.Change(TimeSpan.FromMilliseconds(-1), TimeSpan.FromMilliseconds(-1))
+            Me.IsDeferring = False
         End Sub
         
     #End Region
@@ -91,9 +185,9 @@ Public Class DeferredAction
     #Region "IDisposable Members"
         
         Public Sub Dispose() Implements IDisposable.Dispose
-            If (Me.timer IsNot Nothing) Then
-                Me.timer.Dispose()
-                Me.timer = Nothing
+            If (Me.DeferTimer IsNot Nothing) Then
+                Me.DeferTimer.Dispose()
+                Me.DeferTimer = Nothing
             End If
         End Sub
         
